32f52497c5b6de4f0b7b0bd6040dbb49e9935e80
MoHusseiin <mohamedabdeen898@gmail.com>
2020-03-17 03:58:12 -0500

    init Back end project

---
+++ Back End/MovieCatalogService/src/test/java/com/shika/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shika.moviecatalogservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
